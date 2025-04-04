1a1aaa491e443ba33ea8a09957658995facfcd68
Harsh chaudhary <harshchaudhary@Harshs-MacBook-Pro.local>
2025-04-04 12:52:49 +0530

    deploy-1

--- main.py
+++ main.py
@@ -67,5 +67,6 @@ def rev():
 
 
 if __name__ == '__main__':
-    app.run(debug=True,port=8080)
+    port = int(os.environ.get("PORT", 8080))
+    app.run(debug=True, host="0.0.0.0", port=port)
 print("hello")
